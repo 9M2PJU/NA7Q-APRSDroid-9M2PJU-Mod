0faa9484a73b27ca45618f9b5d6eef168eae50b1
9M2PJU <9m2pju@gmail.com>
2026-07-21 23:43:36 +0800

    fix: use _root_. prefix for PreferenceActivity in Scala

--- src/UIHelper.scala
+++ src/UIHelper.scala
@@ -159,7 +159,7 @@ object UIHelper
 		// preference headers and list items are not drawn under the status
 		// bar. The OnApplyWindowInsetsListener above will refine this if it
 		// fires (e.g. on rotation or multi-window changes).
-		if (act.isInstanceOf[android.preference.PreferenceActivity]) {
+		if (act.isInstanceOf[_root_.android.preference.PreferenceActivity]) {
 			val res = act.getResources()
 			val resId = res.getIdentifier("status_bar_height", "dimen", "android")
 			if (resId > 0) {
